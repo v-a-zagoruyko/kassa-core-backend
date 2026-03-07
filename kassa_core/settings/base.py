c3b8a6a936164ea203c22847a4c0a4b1118c93ce
Васян (OpenClaw) <v.a.zagoruyko.1@gmail.com>
2026-03-07 03:44:52 +0000

    fix: CI удалена лишняя AUTOSLUG_SLUGIFY_FUNCTION из settings, ломавшая тест Store

--- kassa_core/settings/base.py
+++ kassa_core/settings/base.py
@@ -142,7 +142,6 @@ USE_I18N = True
 USE_TZ = True
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-AUTOSLUG_SLUGIFY_FUNCTION = "django.utils.text.slugify"
 DADATA_AUTH_TOKEN = env("DADATA_AUTH_TOKEN", default="dummy-token-for-ci")
 DADATA_REQUEST_TIMEOUT = env.float("DADATA_REQUEST_TIMEOUT", default=5.0)
 
